807e614827020cfe58b3d8794aab29ec2bb2cb76
Hannah Damico <91022356+hannah-damico@users.noreply.github.com>
2022-02-05 13:26:07 -0500

    Second Testing Attempt - Moderate Coverage

--- testing_ehr_analysis.py
+++ testing_ehr_analysis.py
@@ -7,10 +7,10 @@ def testing_parse_data2():
     """Check parse_data2 function coverage."""
     input_file = "/Users/hannahdamico/EHR_PROJECT_821/test_data1.txt"
     data1 = {
-        "key1": ["item11", "item22"],
+        "key1": ["item11", "item21"],
         "key2": ["item12", "item22"],
-        "key3": ["item13", "item32"],
-        "key4": ["item14", "item42"],
+        "key3": ["item13", "item23"],
+        "key4": ["item14", "item24"],
     }
     assert parse_data2(input_file) == data1
 
@@ -27,9 +27,11 @@ def testing_num_older_than():
 def testing_sick_patients():
     """Check sick_patients function coverage."""
     test = parse_data2("/Users/hannahdamico/EHR_PROJECT_821/test_table.txt")
-    check_label = ["HAIUFABG-4543"]
-    check_operation = ["BOAET-64EG"]
+    check_label = set(["HAIUFABG-4543"])
+    check_operation = set(["BOAET-64EG"])
 
     assert sick_patients("METABOLIC: GLUCOSE", ">", 1.0, test) == check_label
-    assert sick_patients("CBC: PLATELET COUNT", "<", 200, test) == 0
-    assert sick_patients("CBC: PLATELET COUNT", "=", 4.5, test) == ValueError
+    assert sick_patients("CBC: PLATELET COUNT", "<", 200, test) == set()
+
+    # with pytest.raises(ValueError):
+    #  sick_patients("CBC: PLATELET COUNT", "=", 4.5, test)
