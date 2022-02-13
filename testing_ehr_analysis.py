08cb80d81b89418c1f5a3e0b91012c259dd9f2f3
Hannah Damico <91022356+hannah-damico@users.noreply.github.com>
2022-02-13 15:21:32 -0500

    Finished README and Testing Functions.

--- testing_ehr_analysis.py
+++ testing_ehr_analysis.py
@@ -1,42 +1,74 @@
 """Testing EHR Analysis."""
 import pytest
-from ehr_analysis import parse_data2, num_older_than, sick_patients
+from ehr_analysis import parse_data, num_older_than, sick_patients, first_admission_age
 
 
-def testing_parse_data2():
+def testing_parse_data():
     """Check parse_data2 function coverage."""
-    input_file = "/Users/hannahdamico/EHR_PROJECT_821/test_data1.txt"
-    data1 = {
+    simple_test_data = "/Users/hannahdamico/EHR_PROJECT_821/test_data1.txt"
+
+    check_simple_test_data = {
         "key1": ["item11", "item21"],
         "key2": ["item12", "item22"],
         "key3": ["item13", "item23"],
         "key4": ["item14", "item24"],
     }
-    assert parse_data2(input_file) == data1
+
+    assert parse_data(simple_test_data) == check_simple_test_data
 
 
 def testing_num_older_than():
     """Check num_older_than function coverage."""
-    test = parse_data2("/Users/hannahdamico/EHR_PROJECT_821/test_data2.txt")
+    patient_core_test_data = parse_data(
+        "/Users/hannahdamico/EHR_PROJECT_821/test_data2.txt"
+    )
 
-    assert num_older_than(-99, test) == 2
-    assert num_older_than(999, test) == 0
-    assert num_older_than(50, test) == 1
+    assert num_older_than(-99, patient_core_test_data) == 2
+    assert num_older_than(999, patient_core_test_data) == 0
+    assert num_older_than(50, patient_core_test_data) == 1
 
 
 def testing_sick_patients():
     """Check sick_patients function coverage."""
-    test = parse_data2("/Users/hannahdamico/EHR_PROJECT_821/test_table.txt")
+    labs_core_test_data = parse_data(
+        "/Users/hannahdamico/EHR_PROJECT_821/test_table.txt"
+    )
     check_label = set(["HAIUFABG-4543"])
     check_operation = set(["BOAET-64EG"])
 
-    assert sick_patients("METABOLIC: GLUCOSE", ">", 1.0, test) == check_label
-    assert sick_patients("CBC: PLATELET COUNT", "<", 200, test) == set()
+    assert (
+        sick_patients("METABOLIC: GLUCOSE", ">", 1.0, labs_core_test_data)
+        == check_label
+    )
+    assert (
+        sick_patients("CBC: PLATELET COUNT", "<", 300, labs_core_test_data)
+        == check_operation
+    )
+    assert sick_patients("CBC: PLATELET COUNT", "<", 200, labs_core_test_data) == set()
     with pytest.raises(ValueError):
-        sick_patients("CBC: PLATELET COUNT", "=", 4.5, test)
+        sick_patients("CBC: PLATELET COUNT", "=", 4.5, labs_core_test_data)
 
 
 def testing_first_admission_age():
     """Check first_admission_age function coverage."""
-    # test =
-    return
+    patient_core_test_data = parse_data(
+        "/Users/hannahdamico/EHR_PROJECT_821/test_data2.txt"
+    )
+    labs_core_test_data = parse_data(
+        "/Users/hannahdamico/EHR_PROJECT_821/test_table.txt"
+    )
+    check_patient_id1 = "HAIUFABG-4543"
+    check_patient_id2 = "BOAET-64EG"
+
+    assert (
+        first_admission_age(
+            check_patient_id1, patient_core_test_data, labs_core_test_data
+        )
+        == 27
+    )
+    assert (
+        first_admission_age(
+            check_patient_id2, patient_core_test_data, labs_core_test_data
+        )
+        == 4
+    )
