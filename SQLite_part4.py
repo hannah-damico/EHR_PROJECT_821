53a0a0e99d053d5e627949a245e546ece746173d
Hannah Damico <91022356+hannah-damico@users.noreply.github.com>
2022-04-19 15:45:51 -0400

    Create Tables & Functions - Fixes Needed.

---
+++ SQLite_part4.py
@@ -0,0 +1,95 @@
+"""
+Part 4 of EHR Cumulative project.
+Implementing SQLite.
+"""
+
+import sqlite3
+import os
+
+if os.path.exists("ehr_data.db"):
+    os.remove("ehr_data.db")
+
+
+con = sqlite3.connect("/Users/hannahdamico/EHR_PROJECT_821/ehr_data.db")
+
+cur = con.cursor()
+
+cur.execute(
+    """CREATE TABLE patient([pat_id] VARCHAR PRIMARY KEY,
+    [gender] VARCHAR,
+    [dob] VARCHAR,
+    [race] VARCHAR,
+    [marital_status] VARCHAR,
+    [language] VARCHAR,
+    [pop_below_poverty] FLOAT)
+    """
+)
+
+cur.execute(
+    """CREATE TABLE labs ([pat_id] VARCHAR NOT NULL,
+    [admission_id] VARCHAR NOT NULL,
+    [lab_name] VARCHAR,
+    [lab_value] FLOAT,
+    [lab_units] VARCHAR,
+    [lab_date_time] VARCHAR,
+    CONSTRAINT lab_id PRIMARY KEY (pat_id, admission_id))
+    """
+)
+
+
+def parse_data_patient(file: str):
+    """Parse Data for Patients."""
+    filename = open(file)
+    next(filename)
+    for row in filename:
+        observations = row.strip().split("\t")
+        data = observations[0:7]
+        cur.execute("INSERT INTO patient VALUES (?, ?, ?, ?, ?, ?, ?)", data)
+
+
+def parse_data_labs(file: str):
+    """Parse Data for Labs."""
+    filename = open(file)
+    next(filename)
+    for row in filename:
+        observations = row.strip().split("\t")
+        data = observations[0:6]
+        cur.execute("INSERT INTO labs VALUES (?, ?, ?, ?, ?, ?)", data)
+
+
+def num_older_than(age: int) -> int:
+    """Compute total number of patients older than input number."""
+    age_list = [age]
+    count_older = cur.execute(
+        "SELECT COUNT (pat_id) FROM patient WHERE (JULIANDAY('now') - JULIANDAY(dob))/365.25 > ?",
+        age_list,
+    )
+    return count_older.fetchall()[0][0]
+
+
+def sick_patients(lab_name: str, value: float, gt_lt: str):
+    """Return a list of patients with specific lab & lab value."""
+    lab_name_match = [lab_name, value]
+    sick_patient_list = cur.execute(
+        f"""SELECT * FROM labs WHERE lab_name = ? AND {gt_lt} ?
+        """,
+        lab_name_match,
+    )
+    return sick_patient_list
+
+
+def first_admission_age(pat_id: str):
+    """Calculate age of patient at first admission."""
+    patient_id = [pat_id]
+    age = cur.execute(
+        """SELECT pat_id FROM patient
+
+    """
+    )
+
+
+# parse_data_patient("patient_core_test_data.txt")
+# parse_data_labs("labs_core_test_data.txt")
+
+print(num_older_than(1))
+print(sick_patients("METABOLIC: GLUCOSE", 2.3, ">"))
--- testing_ehr_analysis.py
+++ testing_ehr_analysis.py
@@ -1,14 +1,12 @@
 """Testing EHR Analysis."""
 import pytest
 from datetime import datetime
-from ehr_analysis import (
+from SQLite_part4 import (
     num_older_than,
-    parse_data_Patient,
-    parse_data_Labs,
+    parse_data_patient,
+    parse_data_labs,
     sick_patients,
     first_admission_age,
-    Patient,
-    Lab,
 )
 
 
@@ -21,8 +19,8 @@ def testing_data_parsing():
     """
 
     simple_test_data = "simple_test_data.txt"
-    patient_core = parse_data_Patient("patient_core_test_data.txt")
-    lab_core = parse_data_Labs("labs_core_test_data.txt")
+    patient_core = parse_data_patient("patient_core_test_data.txt")
+    lab_core = parse_data_labs("labs_core_test_data.txt")
     patient_keys = ["HAIUFABG-4543", "BOAET-64EG"]
 
     assert list(patient_core.keys()) == patient_keys
@@ -32,7 +30,7 @@ def testing_data_parsing():
 
 def testing_num_older_than():
     """Check num_older_than function coverage."""
-    patient_core_test_data = parse_data_Patient("patient_core_test_data.txt")
+    patient_core_test_data = parse_data_patient("patient_core_test_data.txt")
 
     assert num_older_than(-99, patient_core_test_data) == 2
     assert num_older_than(999, patient_core_test_data) == 0
@@ -41,7 +39,7 @@ def testing_num_older_than():
 
 def testing_sick_patients():
     """Check sick_patients function coverage."""
-    labs_core_test_data = parse_data_Labs("labs_core_test_data.txt")
+    labs_core_test_data = parse_data_labs("labs_core_test_data.txt")
     check_label = set(["HAIUFABG-4543"])
     check_operation = set(["BOAET-64EG"])
 
@@ -60,8 +58,8 @@ def testing_sick_patients():
 
 def testing_first_admission_age():
     """Check first_admission_age function coverage."""
-    patient_core_test_data = parse_data_Patient("patient_core_test_data.txt")
-    labs_core_test_data = parse_data_Labs("labs_core_test_data.txt")
+    patient_core_test_data = parse_data_patient("patient_core_test_data.txt")
+    labs_core_test_data = parse_data_labs("labs_core_test_data.txt")
     check_patient_id1 = "HAIUFABG-4543"
     check_patient_id2 = "BOAET-64EG"
 
